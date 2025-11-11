5c11de23639e8fd867d56562e9dbe58c37f218dd
varsha-36 <varshavoundekoti36@gmail.com>
2025-11-11 20:12:55 +0530

    Add files via upload

---
+++ python/Task1-Calculator/README.md
@@ -0,0 +1,12 @@
+# CODSOFT Python Internship – Task 1 (Calculator)
+
+This project is a simple calculator that performs:
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+How to run:
+1. Open terminal/command prompt
+2. Navigate to the folder Task1-Calculator
+3. Run: python main.py
---
+++ python/Task1-Calculator/main.py
@@ -0,0 +1,32 @@
+print("Simple Calculator - CodSoft Internship")
+
+while True:
+    print("\nOperations:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    print("5. Exit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Thank you! Task 1 Completed.")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result:", num1 + num2)
+    elif choice == '2':
+        print("Result:", num1 - num2)
+    elif choice == '3':
+        print("Result:", num1 * num2)
+    elif choice == '4':
+        if num2 == 0:
+            print("Error! Cannot divide by zero.")
+        else:
+            print("Result:", num1 / num2)
+    else:
+        print("Invalid Input")
